d8ca7e22b93ce6c759984d7041b286a3ae30fcff
tomatoYum <tomato6726@gmail.com>
2022-09-07 08:31:21 +0900

    .

---
+++ src/Ps_1330.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ps_1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] a = sc.nextLine().split(" ");
+
+        if(Integer.parseInt(a[0]) == Integer.parseInt(a[1])) {
+            System.out.println("==");
+            return;
+        }
+
+        System.out.println((Integer.parseInt(a[0]) > Integer.parseInt(a[1]) ) ?  ">" : "<");
+
+    }
+}
