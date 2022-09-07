7431445da194fd96343311698b6fe82fe48ebada
tomatoYum <tomato6726@gmail.com>
2022-09-07 11:09:51 +0900

    .

--- src/Ps_2420.java
+++ src/Ps_2420.java
@@ -2,15 +2,15 @@ import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Ps_2420 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         String[] a = sc.nextLine().split(" ");
 
-        int b = Integer.parseInt(a[0]) - Integer.parseInt(a[1]);
+        Long b = Long.parseLong(a[0]) - Long.parseLong(a[1]);
 
-        System.out.println(b);
+        System.out.println((b < 0 ) ? b *= -1 : b );
 
     }
 }
