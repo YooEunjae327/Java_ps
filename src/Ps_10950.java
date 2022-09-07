7f2e660fcab193621fdad4d4f48fd224de375187
tomatoYum <tomato6726@gmail.com>
2022-09-08 08:17:09 +0900

    .

---
+++ src/Ps_10950.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Ps_10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        String[] tokens = new String[1];
+        int[] arr = new int[Integer.parseInt(a)];
+
+        for(int i = 0 ; i < Integer.parseInt(a); i++) {
+            tokens = sc.nextLine().split(" ");
+            arr[i] = Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
+        }
+
+        StringJoiner sj = new StringJoiner("\n");
+        IntStream.of(arr).forEach(x -> sj.add(String.valueOf(x)));
+        System.out.println(sj.toString());
+
+    }
+}
