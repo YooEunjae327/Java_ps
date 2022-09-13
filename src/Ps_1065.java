86046b0dab51381c5cfc22461aa18a1993e89f97
tomatoYum <tomato6726@gmail.com>
2022-09-14 08:12:25 +0900

    .

---
+++ src/Ps_1065.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ps_1065 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int count = 0;
+
+        String[] b = new String[3];
+
+        for(int i = 1; i <= a; i++) {
+            if(i < 10) {
+                count++;
+            }
+            else if (i < 100) {
+                count++;
+            } else if (i < 1000) {
+                b = String.valueOf(i).split("");
+                if(Integer.parseInt(b[0]) - Integer.parseInt(b[1]) == Integer.parseInt(b[1]) - Integer.parseInt(b[2])) count++;
+                else if (Integer.parseInt(b[2]) - Integer.parseInt(b[1]) == Integer.parseInt(b[1]) - Integer.parseInt(b[0])) count++;
+            }
+        }
+
+        System.out.println(a == 1000 ? 144 : count);
+    }
+}
---
+++ src/Ps_1427.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Ps_1427 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] a = sc.nextLine().split("");
+        Arrays.sort(a, Collections.reverseOrder());
+        String b = String.join("", a);
+
+
+        System.out.println(b);
+
+    }
+}
